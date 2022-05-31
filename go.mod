c6a93ed0b7540db999a30b9f50ef5c3b7eaa5a37
Matthew Hodgson <matthew@matrix.org>
2022-05-31 01:12:16 +0100

    remove http API; add basic mautrix VoIP hooks

--- go.mod
+++ go.mod
@@ -5,6 +5,13 @@ go 1.18
 require github.com/pion/webrtc/v3 v3.1.31
 
 require (
+	github.com/pion/rtcp v1.2.9
+	github.com/rs/zerolog v1.26.1
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
+	maunium.net/go/mautrix v0.11.0
+)
+
+require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/pion/datachannel v1.5.2 // indirect
 	github.com/pion/dtls/v2 v2.1.3 // indirect
@@ -13,7 +20,6 @@ require (
 	github.com/pion/logging v0.2.2 // indirect
 	github.com/pion/mdns v0.0.5 // indirect
 	github.com/pion/randutil v0.1.0 // indirect
-	github.com/pion/rtcp v1.2.9 // indirect
 	github.com/pion/rtp v1.7.13 // indirect
 	github.com/pion/sctp v1.8.2 // indirect
 	github.com/pion/sdp/v3 v3.0.4 // indirect
@@ -22,8 +28,8 @@ require (
 	github.com/pion/transport v0.13.0 // indirect
 	github.com/pion/turn/v2 v2.0.8 // indirect
 	github.com/pion/udp v0.1.1 // indirect
-	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838 // indirect
-	golang.org/x/net v0.0.0-20220401154927-543a649e0bdd // indirect
+	golang.org/x/crypto v0.0.0-20220513210258-46612604a0f9 // indirect
+	golang.org/x/net v0.0.0-20220513224357-95641704303c // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- main.go
+++ main.go
@@ -2,72 +2,104 @@ package main
 
 import (
 	"flag"
+	"fmt"
+    "github.com/rs/zerolog/log"
+	yaml "gopkg.in/yaml.v3"
 	"io/ioutil"
-	"log"
-	"net"
-	"net/http"
-	"text/template"
+	"reflect"
+
+	"maunium.net/go/mautrix"
+	"maunium.net/go/mautrix/event"
+	"maunium.net/go/mautrix/id"
 )
 
-func main() {
-	fociCount := flag.Int("foci-count", 4, "How many FOCI should be started")
-	httpAddress := flag.String("http-address", ":8080", "Address for frontend for FOCI cluster")
-	flag.Parse()
-
-	fociPorts := []int{}
-	for i := 0; i < *fociCount; i++ {
-		fociPort, err := createFoci()
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("Starting FOCI on port %d ", fociPort)
-
-		fociPorts = append(fociPorts, fociPort)
+func loadConfig(configFilePath string) (*config, error) {
+	log.Info().Msgf("loaded %s", configFilePath)
+	file, err := ioutil.ReadFile(configFilePath)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to read config")
 	}
-
-	log.Print("Serving HTTP on " + *httpAddress)
-	fileServer := &http.Server{
-		Addr: *httpAddress,
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			indexHTML, err := ioutil.ReadFile("static/index.html")
-			if err != nil {
-				panic(err)
-			}
-
-			if err := template.Must(template.New("").Parse(string(indexHTML))).Execute(w, fociPorts); err != nil {
-				log.Fatal(err)
-			}
-
-		}),
+	var config config
+	if err := yaml.Unmarshal(file, &config); err != nil {
+		return nil, fmt.Errorf("Failed to unmarshal YAML: %s", err)
 	}
-	log.Fatal(fileServer.ListenAndServe())
+	return &config, nil
 }
 
-func createFoci() (int, error) {
-	listener, err := net.Listen("tcp", ":0")
+func main() {
+	configFilePath := flag.String("config", "config.yaml", "Configuration file path")
+	flag.Parse()
+
+	var config *config
+	var err error
+	if config, err = loadConfig(*configFilePath); err != nil {
+		log.Fatal().Err(err).Msg("Failed to load config file")
+	}
+
+	client, err := mautrix.NewClient(config.HomeserverURL, config.UserID, config.AccessToken)
 	if err != nil {
-		return 0, err
+		log.Fatal().Err(err).Msg("Failed to create client")
 	}
 
-	fociServer := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			setCorsHeaders(w)
-			if r.URL.String() != "/createSession" || r.Method != "POST" {
-				return
-			}
+	syncer := client.Syncer.(*mautrix.DefaultSyncer)
+	syncer.ParseEventContent = true
 
-			if err := handleCreateSession(w, r); err != nil {
-				log.Fatal(err)
-			}
+	// add to-device flavours of the call events to mautrix for MSC3401
+	var (
+		CallInvite       = event.Type{"m.call.invite", event.ToDeviceEventType}
+		CallCandidates   = event.Type{"m.call.candidates", event.ToDeviceEventType}
+		CallAnswer       = event.Type{"m.call.answer", event.ToDeviceEventType}
+		CallReject       = event.Type{"m.call.reject", event.ToDeviceEventType}
+		CallSelectAnswer = event.Type{"m.call.select_answer", event.ToDeviceEventType}
+		CallNegotiate    = event.Type{"m.call.negotiate", event.ToDeviceEventType}
+		CallHangup       = event.Type{"m.call.hangup", event.ToDeviceEventType}
+	)
+	event.TypeMap[CallInvite] = reflect.TypeOf(event.CallInviteEventContent{})
+	event.TypeMap[CallCandidates] = reflect.TypeOf(event.CallCandidatesEventContent{})
+	event.TypeMap[CallAnswer] = reflect.TypeOf(event.CallAnswerEventContent{})
+	event.TypeMap[CallReject] = reflect.TypeOf(event.CallRejectEventContent{})
+	event.TypeMap[CallSelectAnswer] = reflect.TypeOf(event.CallSelectAnswerEventContent{})
+	event.TypeMap[CallNegotiate] = reflect.TypeOf(event.CallNegotiateEventContent{})
+	event.TypeMap[CallHangup] = reflect.TypeOf(event.CallHangupEventContent{})
 
-		}),
+	syncer.OnEventType(CallInvite, func(_ mautrix.EventSource, event *event.Event) {
+		log.Info().Interface("event", event)
+	})
+	syncer.OnEventType(CallCandidates, func(_ mautrix.EventSource, event *event.Event) {
+		log.Info().Interface("event", event)
+	})
+	syncer.OnEventType(CallAnswer, func(_ mautrix.EventSource, event *event.Event) {
+		log.Info().Interface("event", event)
+	})
+	syncer.OnEventType(CallReject, func(_ mautrix.EventSource, event *event.Event) {
+		log.Info().Interface("event", event)
+	})
+	syncer.OnEventType(CallSelectAnswer, func(_ mautrix.EventSource, event *event.Event) {
+		log.Info().Interface("event", event)
+	})
+	syncer.OnEventType(CallNegotiate, func(_ mautrix.EventSource, event *event.Event) {
+		log.Info().Interface("event", event)
+	})
+	syncer.OnEventType(CallHangup, func(_ mautrix.EventSource, event *event.Event) {
+		log.Info().Interface("event", event)
+	})
+
+	// TODO: actually hook up events and hook up sessions in the SFU
+	// if err := handleCreateSession(w, r); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	err = client.Sync()
+	if err != nil {
+		log.Panic().Err(err).Msg("Sync failed")
 	}
+}
 
-	go func() {
-		log.Fatal(fociServer.Serve(listener))
-	}()
-
-	return listener.Addr().(*net.TCPAddr).Port, nil
+type config struct {
+	UserID        id.UserID
+	HomeserverURL string
+	AccessToken   string
+	DeviceID      id.DeviceID
 }
 
 type dataChannelMessage struct {
@@ -77,11 +109,4 @@ type dataChannelMessage struct {
 	DeviceID string `json:"device_id"`
 	Purpose  string `json:"purpose"`
 	SDP      string `json:"sdp"`
-}
-
-func setCorsHeaders(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization,X-CSRF-Token")
-	w.Header().Set("Access-Control-Expose-Headers", "Authorization")
 }
