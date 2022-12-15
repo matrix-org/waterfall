99218360da18eac0ad0dafc2298db3e9dc50a61e
Šimon Brandner <simon.bra.ag@gmail.com>
2022-12-15 10:10:48 +0100

    Add `unsubscribe` logic
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- pkg/conference/data_channel_message_processor.go
+++ pkg/conference/data_channel_message_processor.go
@@ -2,7 +2,6 @@ package conference
 
 import (
 	"github.com/pion/webrtc/v3"
-	"github.com/sirupsen/logrus"
 	"golang.org/x/exp/slices"
 	"maunium.net/go/mautrix/event"
 )
@@ -11,35 +10,63 @@ import (
 func (c *Conference) processTrackSubscriptionDCMessage(
 	participant *Participant, msg event.FocusCallTrackSubscriptionEventContent,
 ) {
-	participant.logger.Info("Received track subscription request over DC")
+	participant.logger.Debug("Received track subscription request over DC")
 
-	// TODO: Handle unsubscribe
+	if len(msg.Unsubscribe) != 0 {
+		c.unsubscribeFromTracks(participant, msg.Unsubscribe)
+	}
+	if len(msg.Subscribe) != 0 {
+		c.subscribeToTracks(participant, msg.Subscribe)
+	}
+}
 
-	// Find tracks based on what we were asked for.
-	tracks := c.getTracks(msg.Subscribe)
-
-	participant.logger.WithFields(logrus.Fields{
-		"tracks_we_got":  tracks,
-		"tracks_we_want": msg,
-	}).Debug("Tracks to subscribe to")
+func (c *Conference) unsubscribeFromTracks(participant *Participant, unsubscribe []event.FocusTrackDescription) {
+	// Find tracksToUnsubscribeFrom based on what we were asked for.
+	tracksToUnsubscribeFrom := c.getTracks(unsubscribe)
 
 	// Let's check if we have all the tracks that we were asked for are there.
 	// If not, we will list which are not available (later on we must inform participant
 	// about it unless the participant retries it).
-	if len(tracks) != len(msg.Subscribe) {
-		for _, expected := range msg.Subscribe {
-			found := slices.IndexFunc(tracks, func(track *webrtc.TrackLocalStaticRTP) bool {
+	if len(tracksToUnsubscribeFrom) != len(unsubscribe) {
+		for _, expected := range unsubscribe {
+			found := slices.IndexFunc(tracksToUnsubscribeFrom, func(track *webrtc.TrackLocalStaticRTP) bool {
 				return track.ID() == expected.TrackID
 			})
 
 			if found == -1 {
-				c.logger.Warnf("Track not found: %s", expected.TrackID)
+				c.logger.Warnf("Track to unsubscribe from not found: %s", expected.TrackID)
+			}
+		}
+	}
+
+	// Unsubscribe from the found tracks.
+	for _, track := range tracksToUnsubscribeFrom {
+		participant.logger.WithField("track_id", track.ID()).Debug("Subscribing to track")
+	}
+	participant.peer.UnsubscribeFrom(tracksToUnsubscribeFrom)
+}
+
+func (c *Conference) subscribeToTracks(participant *Participant, subscribe []event.FocusTrackDescription) {
+	// Find tracksToSubscribeTo based on what we were asked for.
+	tracksToSubscribeTo := c.getTracks(subscribe)
+
+	// Let's check if we have all the tracks that we were asked for are there.
+	// If not, we will list which are not available (later on we must inform participant
+	// about it unless the participant retries it).
+	if len(tracksToSubscribeTo) != len(subscribe) {
+		for _, expected := range subscribe {
+			found := slices.IndexFunc(tracksToSubscribeTo, func(track *webrtc.TrackLocalStaticRTP) bool {
+				return track.ID() == expected.TrackID
+			})
+
+			if found == -1 {
+				c.logger.Warnf("Track to subscribe to found: %s", expected.TrackID)
 			}
 		}
 	}
 
 	// Subscribe to the found tracks.
-	for _, track := range tracks {
+	for _, track := range tracksToSubscribeTo {
 		participant.logger.WithField("track_id", track.ID()).Debug("Subscribing to track")
 		if err := participant.peer.SubscribeTo(track); err != nil {
 			participant.logger.Errorf("Failed to subscribe to track: %v", err)
