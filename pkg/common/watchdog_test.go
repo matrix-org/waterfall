69c2c3677f5f207dc02ea87ae20f4435f1878f31
Enrico Schwendig <enrico.schw@gmx.de>
2023-01-16 11:57:45 +0100

    code-style: add line feed at file end, fix comment

--- pkg/common/watchdog_test.go
+++ pkg/common/watchdog_test.go
@@ -14,7 +14,7 @@ func BenchmarkWatchdogChannel_Notify(b *testing.B) {
 	}
 	w := common.StartWatchdog(watchdogConfig)
 
-	// run the Fib function b.N times
+	// run the Notify method b.N times
 	for n := 0; n < b.N; n++ {
 		w.Notify()
 	}
